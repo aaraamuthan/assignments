01631265d5bf000b288dec8e49f280c1cef55cba
Aara Amuthan <aaraamuthanb@gmail.com>
2024-01-16 10:34:39 +0530

    todoList done

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,35 @@
 */
 
 class Todo {
+  constructor() {
+    this.todos = [];
+  }
 
+  add(todo) {
+    this.todos.push(todo);
+  }
+
+  remove(index) {
+    this.todos = this.todos.filter((_, idx) => idx != index);
+  }
+
+  update(index, updatedTodo) {
+    if (index > 0 && index < this.todos.length) this.todos[index] = updatedTodo;
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  get(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length)
+      return this.todos[indexOfTodo];
+    return null;
+  }
+
+  clear() {
+    this.todos = [];
+  }
 }
 
 module.exports = Todo;
