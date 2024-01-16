537dfb64b79d37d88375431a576e558a1c3c8d2a
Aara Amuthan <aaraamuthanb@gmail.com>
2024-01-16 10:17:41 +0530

    countVowels done

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,20 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  const vowels = ["a", "e", "i", "o", "u"];
+  let cnt = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i].toLowerCase() == "a" ||
+      str[i].toLowerCase() == "e" ||
+      str[i].toLowerCase() == "i" ||
+      str[i].toLowerCase() == "o" ||
+      str[i].toLowerCase() == "u"
+    ) {
+      cnt++;
+    }
+  }
+  return cnt;
 }
 
 module.exports = countVowels;
